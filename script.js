c7a334fd4ba263f712ce0ec555829f2c1cb9fbd1
Judexify <olukojuoluwadunsi12@gmail.com>
2026-01-20 23:16:29 +0100

    learn Async with me

--- script.js
+++ script.js
@@ -79,41 +79,177 @@ const renderCountry = function (data, className = '') {
 // };
 // getCountryData('portugal');
 
-const renderError = function (msg) {
-  countriesContainer.insertAdjacentText('beforeend', msg);
-};
+// const renderError = function (msg) {
+//   countriesContainer.insertAdjacentText('beforeend', msg);
+// };
 
-const getJSON = function (url, errorMsg = 'Something Went Wrong') {
-  return fetch(url).then(response => {
-    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
-    return response.json();
+// const getJSON = function (url, errorMsg = 'Something Went Wrong') {
+//   return fetch(url).then(response => {
+//     if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+//     return response.json();
+//   });
+// };
+
+// const getCountryData = country => {
+//   // prettier-ignore
+//   getJSON(`https://restcountries.com/v2/name/${country}`, 'Country not found')
+//     .then(data => {
+//       renderCountry(data[0]);
+//       const neighbour = data[0].borders[0];
+//       if (!neighbour) return;
+
+//       // country 2
+
+//       return getJSON(`https://restcountries.com/v2/alpha/${neighbour}`).then(
+//         data => renderCountry(data, 'neighbour'),
+//       );
+//     })
+
+//     .catch(err => {
+//       renderError(`Something went wrong 💥💥💥${err.message}. Try Again! `);
+//       console.error(`${err} 💥💥💥`);
+//     })
+//     .finally(() => {
+//       countriesContainer.style.opacity = 1;
+//     });
+// };
+
+// btn.addEventListener('click', () => {
+//   getCountryData('china');
+// });
+
+// const whereAmI = (lat, lng) => {
+//   fetch(
+//     `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}`,
+//   )
+//     .then(response => {
+//       if (!response.ok) throw new Error(`${response.status}`);
+//       return response.json();
+//     })
+//     .then(data => {
+//       console.log(`You are in ${data.locality}`);
+//     })
+//     .catch(err => console.error(`${err.message} 💥💥💥`));
+// };
+// whereAmI(52.508, 13.381);
+// whereAmI(19.037, 72.873);
+// whereAmI(-33.933, 18.474);
+
+// console.log('Test Start');
+// setTimeout(() => console.log('0 sec Timer'), 0);
+// Promise.resolve('Resolved Promise 1').then(res => console.log(res));
+// Promise.resolve('Resolved Promise 2').then(res => {
+//   for (let i = 0; i < 1000000000; i++) {}
+//   console.log(res);
+// });
+// console.log('Test End');
+
+// building a simple promise
+
+// const lotteryPromise = new Promise((resolve, reject) => {
+//   // an executor fn goes here
+//   console.log('Lottery draw is happening');
+//   setTimeout(() => {
+//     if (Math.random() >= 0.5) {
+//       resolve('You WIN 💰💰');
+//     } else {
+//       reject(new Error('You Lost your money 💩'));
+//     }
+//   }, 2000);
+// });
+
+// // consuming promise => convert call back based function to promises
+// lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
+
+// // promisifying
+const wait = function (seconds) {
+  return new Promise(resolve => {
+    setTimeout(resolve, seconds * 1000);
   });
 };
 
-const getCountryData = country => {
-  // prettier-ignore
-  getJSON(`https://restcountries.com/v2/name/${country}`, 'Country not found')
-    .then(data => {
-      renderCountry(data[0]);
-      const neighbour = data[0].borders[0];
-      if (!neighbour) return;
+// wait(2).then(() => {
+//   console.log('i waited for 2 seconds');
+//   return wait(1);
+// });
 
-      // country 2
+// lets promisify Geolocation
 
-      return getJSON(`https://restcountries.com/v2/alpha/${neighbour}`).then(
-        data => renderCountry(data, 'neighbour'),
-      );
-    })
+// console.log('getting position....');
 
-    .catch(err => {
-      renderError(`Something went wrong 💥💥💥${err.message}. Try Again! `);
-      console.error(`${err} 💥💥💥`);
-    })
-    .finally(() => {
-      countriesContainer.style.opacity = 1;
-    });
+const getPosition = () => {
+  return new Promise(function (resolve, reject) {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
 };
 
-btn.addEventListener('click', () => {
-  getCountryData('china');
-});
+const whereAmI = (lat, lng) => {
+  getPosition()
+    .then(pos => {
+      const { latitude: lat, longitude: lng } = pos.coords;
+      return fetch(
+        `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}`,
+      );
+    })
+    .then(response => {
+      if (!response.ok) throw new Error(`${response.status}`);
+      return response.json();
+    })
+    .then(data => {
+      console.log(data);
+      console.log(`You are in ${data.city}, ${data.countryName}`);
+      return fetch(`https://restcountries.com/v2/name/${data.countryName}`);
+    })
+    .then(res => {
+      if (!res.ok) throw new Error(`Country not found (${res.status})`);
+      return res.json();
+    })
+    .then(data => renderCountry(data[0]))
+    .catch(err => console.error(`${err.message} 💥💥💥`));
+};
+
+// btn.addEventListener('click', whereAmI);
+
+// CHALLENGE 2
+// const getImage = () => {
+//   return new Promise((resolve, reject) => {
+
+//   });
+// };
+
+const createImage = imagePath => {
+  return new Promise((resolve, reject) => {
+    const newImage = document.createElement('img');
+    newImage.src = imagePath;
+
+    const imageDiv = document.querySelector('.images');
+    if (imageDiv) {
+      newImage.addEventListener('load', () => {
+        imageDiv.appendChild(newImage);
+      });
+    }
+    resolve(newImage);
+
+    newImage.addEventListener('error', () => {
+      reject(new Error('Image not found...'));
+    });
+  });
+};
+
+let currentImg;
+createImage('/img/img-1.jpg')
+  .then(img => {
+    currentImg = img;
+    console.log('Image 1 Loaded....');
+    return wait(2);
+  })
+  .then(() => {
+    currentImg.style.display = 'none';
+    return createImage('/img/img-2.jpg');
+  })
+  .then(img => {
+    currentImg = img;
+    console.log('Image 2 Loaded....');
+    return wait(2);
+  })
+  .catch(err => console.error(err));
